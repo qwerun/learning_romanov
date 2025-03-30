9ddf7f75158103ce566387f7707524d4399cb375
qwerun <diger0912@mail.ru>
2025-03-31 00:13:34 +0300

    weak6 start

---
+++ weak6/db_explorer/main.go
@@ -0,0 +1,37 @@
+// тут лежит тестовый код
+// менять вам может потребоваться только коннект к базе
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	// DSN это соединение с базой
+	// вы можете изменить этот на тот который вам нужен
+	// docker run -p 3306:3306 -v $(PWD):/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=golang -d mysql
+	// DSN = "root@tcp(localhost:3306)/golang2017?charset=utf8"
+	DSN = "root:love@tcp(localhost:3306)/photolist?charset=utf8"
+)
+
+func main() {
+	db, err := sql.Open("mysql", DSN)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping() // вот тут будет первое подключение к базе
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("GJ")
+
+	//handler, err := NewDbExplorer(db)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println("starting server at :8082")
+	//http.ListenAndServe(":8082", handler)
+}
