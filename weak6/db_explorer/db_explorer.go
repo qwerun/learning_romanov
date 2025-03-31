481b5102d48aa9d53e72c5c9e2dce2c5699604bf
qwerun <diger0912@mail.ru>
2025-04-01 02:41:10 +0300

    work2 weak6

--- weak6/db_explorer/db_explorer.go
+++ weak6/db_explorer/db_explorer.go
@@ -7,26 +7,35 @@ import (
 )
 
 type Table struct {
+	Name    string `json:"name"`
+	Columns []Column
+}
+
+type Column struct {
 	Name string `json:"name"`
 }
 
-func jsonResponse(r *http.ResponseWriter, text any, st int) {
+func writeJson(w http.ResponseWriter, v any, status int) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
 
+func writeErrJson(w http.ResponseWriter, err error, status int) error {
+	result := map[string]any{
+		"ok":    false,
+		"error": err.Error(),
+	}
+	return writeJson(w, result, status)
 }
 
 func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
 		if r.URL.Path == "/" {
 			var tables []Table
 			rows, err := db.Query("SHOW TABLES;")
 			if err != nil {
-				result := map[string]any{
-					"ok":    false,
-					"error": err.Error(),
-				}
-				json.NewEncoder(w).Encode(result)
-				w.WriteHeader(http.StatusInternalServerError)
+				_ = writeErrJson(w, err, http.StatusInternalServerError)
 				return
 			}
 
@@ -34,19 +43,17 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 				var table Table
 				err := rows.Scan(&table.Name)
 				if err != nil {
-					panic(err)
+					_ = writeErrJson(w, err, http.StatusInternalServerError)
+					return
 				}
 				tables = append(tables, table)
 			}
 
-			jsonData, err := json.Marshal(tables)
+			err = writeJson(w, tables, http.StatusOK)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("Error encoding JSON"))
+				_ = writeErrJson(w, err, http.StatusInternalServerError)
 				return
 			}
-
-			w.Write(jsonData)
 			return
 		}
 
@@ -56,5 +63,4 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 		}
 
 	}), nil
-	return nil, nil
 }
